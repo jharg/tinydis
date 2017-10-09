41ee268a0a7641835821d0d53e67e04b1c821f1a
Jordan Hargrave <jordan_hargrave@dell.com>
2017-10-08 22:40:18 -0500

    Move SH4 definitions to header file

---
+++ sh4.h
@@ -0,0 +1,71 @@
+#ifndef __sh4_h__
+#define __sh4_h__
+
+enum {
+  SH4_BASE = 0x4000,
+  Rm,
+  Rn,
+  FRm,
+  FRn,
+  i8,
+  i20,
+  Pr15, // [R15++]
+
+  Dn,   // [--Rn]
+  Pn,   // [Rn++]
+  dn,   // [Rn]
+  dn0,  // [Rn+R0]
+  d4n,  // [Rn+d4]
+  d12n, // [Rn+d12]
+
+  Dm,   // [--Rm]
+  Pm,   // [Rm++]
+  dm,   // [Rm]
+  dm0,  // [Rm+R0]
+  d4m,  // [Rm+d4]
+  d12m, // [Rm+d12]
+  dmp,  // [Rm+PC]
+
+  d8g,  // [GBR+d8]
+  dr0g, // [GBR+R0]
+  d8p,  // [PC+d8]
+
+  j8,
+  j12,
+
+  _fT,
+  _fS,
+  _fM,
+  _fQ,
+
+  SIZE_FLOAT = 'f' << SIZE_SHIFT,
+
+  R0  = TYPE_REG+SIZE_DWORD,
+  R15 = TYPE_REG+SIZE_DWORD+15,
+  PC  = TYPE_REG+SIZE_DWORD+16,
+
+  rMACH,
+  rMACL,
+  rPR,
+  rSGR,
+  rDSR,
+  rFPUL,
+  rA0,
+  rX0,
+  rX1,
+  rY0,
+  rY1,
+  rDBR,
+  rGBR,
+  rVBR,
+  rTBR,
+  rSSR,
+  rSR,
+  rSPC,
+  rMOD,
+  rRS,
+  rRE,
+};
+
+
+#endif
